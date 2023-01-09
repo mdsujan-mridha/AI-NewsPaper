a844bbd024ad850913dc2e01334e494e836e5818
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-01-10 03:46:04 +0600

    update scroll

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './componrnts/Home/Home';
 import NavBar from './componrnts/NavBar/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import Contact from './componrnts/Contact/Contact';
+import wordsToNumbers from 'words-to-numbers';
 import Swiper from 'swiper';
 import 'swiper/css';
 import Footer from './componrnts/Footer/Footer';
@@ -15,33 +16,45 @@ import Footer from './componrnts/Footer/Footer';
 const alanKey = 'b32e2a27099709684e14d730e8b02add2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 function App() {
-  const[newsArticles , setNewsArticles] = useState([]);
-  const[activeArticle,setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
 
 
-  useEffect( ()=>{
+  useEffect(() => {
     alanBtn({
-     key:alanKey,
-     onCommand:({ command,articles })=>{
-          if(command === 'newHeadlines'){
-            setNewsArticles(articles);
-          }else if(command === 'highlight'){
-            setActiveArticle((prevActiveArticle) =>prevActiveArticle +1);
+      key: alanKey,
+      onCommand: ({ command, articles,number  }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
           }
-     }
+        }
+      }
     })
-  } ,[])
+  }, [])
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
       </Routes>
 
-      <NewsCard newsArticles={newsArticles} activeArticle={activeArticle}/>
-      <Footer/>
+      <NewsCard newsArticles={newsArticles} activeArticle={activeArticle} />
+      <Footer />
     </div>
   );
 }
--- src/componrnts/NewsCards/NewsCards.js
+++ src/componrnts/NewsCards/NewsCards.js
@@ -1,14 +1,31 @@
 
-import React from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const NewsCards = ({ article, i,activeArticle }) => {
-    console.log(article);
+const NewsCards = ({ article, i, activeArticle }) => {
+    // console.log(article);
+    const [elRefs, setElRefs] = useState([]);
     const { author, content, description, publishedAt, title, url, urlToImage, source
     } = article;
 
+    //    for auto scroll 
+    
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        window.scroll(0, 0);
+
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if (i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
     return (
-        <a href={url} target="_blank" className="card card-compact  bg-base-100 shadow-2xl">
+        <a ref={elRefs[i]} href={url} target="_blank" className="card card-compact  bg-base-100 shadow-2xl">
             <figure>
                 {
                     urlToImage ? <img src={urlToImage} alt="NEWS" /> : <img src='https://i.ibb.co/gw3rBRL/news-icon-18.png' alt='news'></img>
@@ -16,18 +33,20 @@ const NewsCards = ({ article, i,activeArticle }) => {
             </figure>
             <div className="card-body">
                 <div className='flex justify-between text-secondary font-semibold'>
-                    <p> {(new Date(publishedAt)).toDateString()} </p> <p className='opacity-50'> {source?.name} </p>
+                    <p> {(new Date(publishedAt)).toDateString()} </p> 
+                    <p className='opacity-50'> {source?.name} </p>
+                    <p className='text-2xl'> {i+1} </p>
                 </div>
 
                 <h2 className="card-title text-justify font-bold text-white"> {title} </h2>
                 <p className='text-justify font-semibold text-white opacity-75'> {description} </p>
-                <div className="card-actions">
+                <div className="flex justify-between">
                     <p className='text-secondary font-bold'> author:
                         {
                             author ? author : source?.name
                         } </p>
                     <a to={url} target="_blank" className="btn btn-sm btn-primary"> Read more... </a>
-
+                    
                 </div>
             </div>
         </a>
